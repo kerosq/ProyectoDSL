ec5ae0d2c45b9cdf82e0d2a4d4c9a764b59ccac2
Kevin <kevin.cunyas.quispe@alumnos.upm.es>
2021-06-10 22:36:48 +0200

    update TT

--- Debugging/LineaPedido.java
+++ Debugging/LineaPedido.java
@@ -5,5 +5,5 @@ public int LineaPedidoID;
 public int getLineaPedidoID(int LineaPedidoID){return 0;}
 public void setLineaPedidoID(int LineaPedidoID){ this.LineaPedidoID=LineaPedidoID;}
 public List <Producto> Tiene_Producto;
-public LineaPedido(List <Producto> Tiene_Producto){}
+public LineaPedido(public List <Producto> Tiene_Producto;){}
 }
